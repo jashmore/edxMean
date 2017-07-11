e52adfbc0839032ae142372941aa75036eaf5b39
jashmore <john@xforge.co.uk>
2017-07-11 16:34:12 +0100

    Set up project.

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by john on 11/07/17.
+ */
+/**
+ * Created by john on 11/07/17.
+ */
+var _ = require('underscore')
+
+_.each( [1,2,3], function(v) {
+    console.log(v);
+});
---
+++ server.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by john on 11/07/17.
+ */
+import _ = require('underscore')
+
+_.each( [1,2,3], (v: number): void => {
+    console.log(v);
+});
